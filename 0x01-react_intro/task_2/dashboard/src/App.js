368a11e5c58d433a92afae3cd423cfc0cb53718d
Yofty <106748943+Yofty@users.noreply.github.com>
2023-06-03 13:44:24 +0300

    task_4

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -4,7 +4,7 @@ import { getFullYear, getFooterCopy } from './utils.js'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App'>      
       <div className="App-header">
         <img src={holbertonlogo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
