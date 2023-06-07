5628a14e8345f3f54ea12814c1bf97e4f7973db2
Yofty <106748943+Yofty@users.noreply.github.com>
2023-06-07 17:36:28 +0300

    task_1

--- 0x04-React_inline_styling/task_4/dashboard/src/Footer/Footer.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/Footer/Footer.js
@@ -1,13 +1,12 @@
-import React from "react";
-import "./Footer.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
+import React from 'react';
+import { getFooterCopy, getFullYear } from '../utils/utils';
 
 function Footer() {
   return (
     <div className="App-footer">
       <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
-    </div>    
-  );
+    </div>
+  )
 }
 
 export default Footer;
--- 0x04-React_inline_styling/task_4/dashboard/src/Footer/Footer.test.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/Footer/Footer.test.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Footer from './Footer';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 describe("Testing <Footer /> component", () => {
-    let wrapper;
+  let wrapper;
 
-    beforeEach(() => {
-        wrapper = shallow(<Footer />);
-    });
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = shallow(<Footer />);
+  });
 
-    it("Footer Component renders without crashing", () => {
-        expect(wrapper.exists());
-    });
+  it("Footer Component renders without crashing", () => {
+    expect(wrapper.exists());
+  });
 
-    it("Footer compoenent render at the very least the text “Copyright”", () => {
-        expect(wrapper.find("Copyright").at(0)).toBeDefined();
-    });
+  it("Footer compoenent render at the very least the text “Copyright”", () => {
+    expect(wrapper.find("Copyright").at(0)).toBeDefined();
+  });
 });
