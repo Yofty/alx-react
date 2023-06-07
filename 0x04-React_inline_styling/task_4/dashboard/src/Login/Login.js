b13fccf93330465ee2b4e550b941364742fc1d19
Yofty <106748943+Yofty@users.noreply.github.com>
2023-06-07 17:13:47 +0300

    task_4

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, css }from "aphrodite";
+
+function Login() {
+  return (
+    <React.Fragment> 
+      <div className={css(styles["App-body"])}>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" className={loginStyles.inputs} />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" className={loginStyles.inputs} />
+        <button>OK</button>
+      </div>
+    </React.Fragment>
+  );
+}
+
+const loginStyles = StyleSheet.create({
+  "App-body": {
+    padding: '36px 24px',
+    '@media (max-width: 900px)': {
+      display: 'flex',
+      flexDirection: 'column'
+    }
+  },
+
+  input: {
+    margin: '0 16px 0 8px',
+  },
+});
+
+export default Login;
