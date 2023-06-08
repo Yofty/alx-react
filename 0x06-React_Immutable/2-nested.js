a1245262bb0c0232717fa48abac00a6e35cb16c3
Yofty <106748943+Yofty@users.noreply.github.com>
2023-06-08 11:53:47 +0000

    task 2 -4

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { formJS } from 'immutable';
+
+export default function accessImmutableObject(object, array) {
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
+}
