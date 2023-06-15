02b7d1ceab35700b3c8951ac709dc423566c7fbf
Yofty <106748943+Yofty@users.noreply.github.com>
2023-06-15 15:49:45 +0300

    task 5

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,27 @@
+import {
+    LOGIN,
+    LOGOUT,
+    DISPLAY_NOTIFICATION_DRAWER,
+    HIDE_NOTIFICATION_DRAWER,
+} from './uiActionTypes.js';
+
+export const login = (email, password) => {
+    return {
+        type: LOGIN,
+        user: { email, password },
+    };
+};
+
+export const logout = () => {
+    return {
+        type: LOGOUT,
+    };
+};
+
+export const displyNotificationDrawer = () => {
+    return { type: DISPLAY_NOTIFICATION_DRAWER };
+};
+
+export const hideNotificationDrawer = () => {
+    return { type: HIDE_NOTIFICATION_DRAWER };
+};
